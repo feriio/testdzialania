11605792444b882cbd921ed6505235585a0cedd4
feriio <91228778+feriio@users.noreply.github.com>
2021-09-24 01:46:32 +0200

    useragent
    update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $curl_handle=curl_init();
 curl_setopt($curl_handle, CURLOPT_URL,'https://bilety.legia.com/Stadium/GetWGLSectorsInfo?eventId=701');
 curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
 curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0 Waterfox/78.14.0');
 $query = curl_exec($curl_handle);
 curl_close($curl_handle);
 echo $query;
